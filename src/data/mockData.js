60fda6013d962a45ea211d8bac3d7e7e5eba76cf
Soumyajit Saha <soumyajitsa@firstsource.com>
2026-06-24 12:22:43 +0530

    Restore and update local project fil

--- src/data/mockData.js
+++ src/data/mockData.js
@@ -198,7 +198,7 @@ export const mockReturnedChecksCashPosting = [
     returnReason: 'Overpayment - Ambulance Transport',
     receivedDate: '2024-03-22',
     status: 'new',
-    pdfUrl: '/Emergency Ambulance Service Inc.pdf'
+    pdfUrl: '/redacted_0000024212 $489.48.pdf'
   },
   {
     id: 'CHK004',
